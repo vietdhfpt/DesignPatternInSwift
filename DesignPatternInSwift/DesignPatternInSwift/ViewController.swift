1441cc449a69977bb7c01fcd0d9836bf5789de9c
DHV <dhv@gmail.com>
2020-10-26 10:39:53 +0700

    Init project

---
+++ DesignPatternInSwift/DesignPatternInSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesignPatternInSwift
+//
+//  Created by Gia Han on 10/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
